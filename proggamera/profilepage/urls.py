16d725a3c390e83a4b84dc42ef8b442550cfe001
PhilipGullberg <philip_gullberg@hotmail.com>
2022-03-02 20:09:22 +0100

    commit on home

--- proggamera/profilepage/urls.py
+++ proggamera/profilepage/urls.py
@@ -8,14 +8,17 @@ urlpatterns = [
     path('teacher/', views.teacherdash, name="teacher_dash"),
     path('student/', views.studentdash, name="student_dash"),
     path('student/classroom', views.s_classroom, name="s_classroom"),
+    path('student/classroom/add/<int:pk>',views.add_student_to_classroom, name="student_code"),
     path('student/courses', views.s_courses, name="s_courses"),
     path('teacher/classroom/', views.t_classroom, name="t_classroom"),
     path('teacher/results',views.t_results, name="t_results"),
     path('teacher/classroom/<int:pk>', views.curr_classroom, name="curr_classroom"),
     path('teacher/classroom/<int:pk>/add', views.add_students, name="add_students"),
+    path('teacher/classroom/<int:classroomid>/add/code', views.add_students_code, name="add_students_code"),
     path('teacher/classroom/<int:pk>/search_students/', views.search_students, name="search_students"),
     path('teacher/classroom/<int:pk>/search_students/added/<int:student>', views.added, name="added"),
     path('teacher/classroom/<int:classroom>/<str:student>', views.remove_student , name="remove_student"),
+    
     
 
 
--- proggamera/proggamera/urls.py
+++ proggamera/proggamera/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('profilepage/', include('profilepage.urls')),
     path('courses/', include('courses.urls')),
+
     
 ]
 
